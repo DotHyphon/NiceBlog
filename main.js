3513b6d3452dcc14f81c7d96cabdacb7698e75aa
William Dean <William.dean93@gmail.com>
2023-04-13 12:30:10 +1000

    added bootstrap again

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-// Replace the placeholders with your database details
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'your_username',
-    password: 'your_password',
-    database: 'your_database_name'
-});
+// // Replace the placeholders with your database details
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'your_username',
+//     password: 'your_password',
+//     database: 'your_database_name'
+// });
 
-signUp();
+// signUp();
 
 
-function signUp() {
-    connection.connect();
+// function signUp() {
+//     connection.connect();
 
-    // Define the SQL query using a parameterized query
-    const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+//     // Define the SQL query using a parameterized query
+//     const sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
 
-    // Define the user data as an array
-    const userData = ['John Doe', 'john@example.com', 'password123'];
+//     // Define the user data as an array
+//     const userData = ['John Doe', 'john@example.com', 'password123'];
 
-    // Execute the parameterized query with the user data
-    connection.query(sql, userData, function (error, results, fields) {
-        if (error) throw error;
-        console.log('New record created successfully');
-    });
+//     // Execute the parameterized query with the user data
+//     connection.query(sql, userData, function (error, results, fields) {
+//         if (error) throw error;
+//         console.log('New record created successfully');
+//     });
 
-    connection.end();
-    console.log('Connection closed');
-}
+//     connection.end();
+//     console.log('Connection closed');
+// }
